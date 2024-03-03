8848572cc12a6b117eccf23c8d22a7383fce2e84
Chandan-Alimilli <chandanalimilli248@gmail.com>
2024-03-03 23:00:27 +0530

    new

--- Natours/controllers/bookingController.js
+++ Natours/controllers/bookingController.js
@@ -10,7 +10,8 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 export const getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  // console.log(tour);
+
+console.log(tour)
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
